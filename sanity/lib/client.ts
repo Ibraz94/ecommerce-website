fe2ca8c00a05cddc00db73c9a1d142e966a1a368
Ibraz94 <150535177+Ibraz94@users.noreply.github.com>
2025-01-20 18:18:59 +0500

    Fifth Commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,9 +6,4 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: process.env.NODE_ENV === "production",
-  stega: {
-    studioUrl: process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}/studio`
-    : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
-  }
-})
+  });
