3440d7b274c7f35e76e5da12887c48aff863ff56
Ibraz94 <150535177+Ibraz94@users.noreply.github.com>
2025-01-20 22:00:29 +0500

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity';
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "i7r8upi1",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  useCdn: true,
   apiVersion:"2025-01-20",
   token: process.env.SANITY_API_READ_TOKEN,
+  useCdn: process.env.NODE_ENV === 'production'
   });
 
   export default client;
