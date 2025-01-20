c97742d6ea8465212111f1ca4534be5919d512d6
Ibraz94 <150535177+Ibraz94@users.noreply.github.com>
2025-01-20 18:30:30 +0500

    Sixth Commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,10 @@
-import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from 'next-sanity';
 
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: process.env.NODE_ENV === "production",
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "i7r8upi1",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  useCdn: true,
+  apiVersion:"2025-01-20",
   });
+
+  export default client;
