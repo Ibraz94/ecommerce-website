33e78981dd49e641e9e2ff823a9363ab8de8c4f9
Ibraz94 <150535177+Ibraz94@users.noreply.github.com>
2025-01-22 23:26:13 +0500

    Update page.tsx

--- app/(store)/categories/[slug]/page.tsx
+++ app/(store)/categories/[slug]/page.tsx
@@ -2,9 +2,13 @@ import ProductsView from "@/app/components/ProductsView";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getProductByCategory } from "@/sanity/lib/products/getProductByCategory";
 
-async function CategoryPage(
-    { params }: { params: Promise<{ slug: string }> }
-) {
+async function CategoryPage({
+   params 
+  }: { 
+    params: Promise<{ 
+    slug: string }> 
+  }) 
+  {
     const { slug } = await params;
     const products = await getProductByCategory(slug);
     const categories = await getAllCategories();
